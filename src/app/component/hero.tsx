7abcec4cd4a1b1d2cb5eb81578e3b5ec7179c119
MuhammadFahadSiddiqui <162031059+MuhammadFahadSiddiqui@users.noreply.github.com>
2024-10-20 04:45:31 -0700

    Update hero.tsx

--- src/app/component/hero.tsx
+++ src/app/component/hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-5xl text-3xl mb-4 font-medium text-white">
-              Hi! <span className="text-cyan-400 font-medium">&apos;I'm,&apos;</span>
+              Hi! <span className="text-cyan-400 font-medium">I'm,</span>
               <br className="hidden lg:inline-block text-cyan" />
               <Typewriter
                 options={{
